3719cc6211d03062dd19525081728404e2d18724
worksoup <2055038075@qq.com>
2024-02-07 16:44:00 +0800

    确保可以编译。

--- src/lib.rs
+++ src/lib.rs
@@ -1,38 +1,39 @@
 use std::collections::HashMap;
 use j4rs::{Instance, InvocationArg, Jvm};
+use mirai_j4rs::mj_base::env::FromInstanceTrait;
 use mirai_j4rs::utils::MiraiMap;
 use mirai_j4rs::utils::other::enums::MiraiProtocol;
 
 pub struct ExtraTest;
 
 impl ExtraTest {
-    pub fn fetch(protocol: &MiraiProtocol, version: String) -> Box<dyn FnOnce(&Jvm, &Instance, &Instance)> {
-        Box::new(|jvm: &Jvm, builder: &Instance, config: &Instance| {
+    pub fn fetch(protocol: MiraiProtocol, version: String) -> Box<dyn FnOnce(&Jvm, &Instance, &Instance)> {
+        Box::new(move |jvm: &Jvm, _builder: &Instance, _config: &Instance| {
             println!("extra test - tmp - fetch");
             let _ = jvm.invoke_static(
                 "xyz.cssxsh.mirai.tool.FixProtocolVersion",
                 "fetch",
                 &[
-                    mirai_j4rs::utils::other::protocol_enum_r2j(protocol).unwrap(),
+                    mirai_j4rs::utils::other::protocol_enum_r2j(&protocol).unwrap(),
                     InvocationArg::try_from(version).unwrap(),
                 ],
             );
         })
     }
 
-    pub fn load(protocol: &MiraiProtocol) -> Box<dyn FnOnce(&Jvm, &Instance, &Instance)> {
-        Box::new(|jvm: &Jvm, builder: &Instance, config: &Instance| {
+    pub fn load(protocol: MiraiProtocol) -> Box<dyn FnOnce(&Jvm, &Instance, &Instance)> {
+        Box::new(move |jvm: &Jvm, _builder: &Instance, _config: &Instance| {
             println!("extra test - tmp - load");
             let _ = jvm.invoke_static(
                 "xyz.cssxsh.mirai.tool.FixProtocolVersion",
                 "load",
-                &[mirai_j4rs::utils::other::protocol_enum_r2j(protocol).unwrap()],
+                &[mirai_j4rs::utils::other::protocol_enum_r2j(&protocol).unwrap()],
             );
         })
     }
 
-    pub fn load_form_file(protocol: &MiraiProtocol, file: &str) -> Box<dyn FnOnce(&Jvm, &Instance, &Instance)> {
-        Box::new(|jvm: &Jvm, builder: &Instance, config: &Instance| {
+    pub fn load_form_file(protocol: MiraiProtocol, file: String) -> Box<dyn FnOnce(&Jvm, &Instance, &Instance)> {
+        Box::new(move |jvm: &Jvm, _builder: &Instance, _config: &Instance| {
             println!("extra test - tmp - load");
             let file = InvocationArg::try_from(
                 jvm
@@ -44,7 +45,7 @@ impl ExtraTest {
                 "xyz.cssxsh.mirai.tool.FixProtocolVersion",
                 "load",
                 &[
-                    mirai_j4rs::utils::other::protocol_enum_r2j(protocol).unwrap(),
+                    mirai_j4rs::utils::other::protocol_enum_r2j(&protocol).unwrap(),
                     file,
                 ],
             );
